b0eee863e8e5782a9e12fcfec66606fb9b26e5bd
Dominika Drela <61236771+DrelaDominika@users.noreply.github.com>
2021-03-06 14:06:54 +0100

    Add files via upload

---
+++ Loops/Projekt1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Projekt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=100; i>1;i--)
+            {
+                if(i % 3 == 0 && i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                   
+                }
+            }
+            int a = 100;
+            while (a > 1)
+            {
+                if (a % 3 == 0 && a % 2 != 0)
+                {
+                    Console.WriteLine(a);
+                   
+                }
+                a--;
+            }
+            int b = 100;
+            do
+            {
+                if (b % 3 == 0 && b % 2 != 0)
+                {
+                    Console.WriteLine(b);
+
+                }
+                b--;
+            } while (b > 1);
+            
+            Console.ReadKey();
+
+        }
+    }
+}
