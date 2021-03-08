6b37dfebdcd78cd25586a08082a387366237bee1
Dominika Drela <61236771+DrelaDominika@users.noreply.github.com>
2021-03-08 16:16:35 +0100

    Add files via upload

---
+++ Fibonacci/Projekt2a/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Projekt2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] tab = new int[10][];
+
+            int x = 0;
+            int y = 1;
+
+            for(int i = 0; i < 10; i++)
+            {
+                tab[i] = new int[y];
+                for(int j = 0; j < tab[i].Length; j++)
+                {
+                    tab[i][j] = tab[i].Length - j;
+                }
+                int z = x + y;
+                x = y;
+                y = z;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("tab({0}): ", i);
+                for (int j = 0; j < tab[i].Length; j++)
+                {
+                    Console.Write(tab[i][j] + " ");
+
+                }
+
+                Console.WriteLine();
+
+            }
+
+        }
+        
+    }
+}
