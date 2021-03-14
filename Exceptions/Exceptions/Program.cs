1a166918ed3d527d6f4b86b3832c006952107379
Dominika Drela <61236771+DrelaDominika@users.noreply.github.com>
2021-03-14 11:52:38 +0100

    Add files via upload

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArithmeticException
+            try
+            {
+                Console.WriteLine("Podaj pierwsza liczbe:");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Podaj druga liczbe:");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(a / b);
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine("Nie mozna dzielic przez 0!", e);
+            }
+
+            //Exception
+            catch (FormatException e)
+            {
+                Console.WriteLine("Podano niepoprawny format!", e);
+            }
+           
+        }
+    }
+}
